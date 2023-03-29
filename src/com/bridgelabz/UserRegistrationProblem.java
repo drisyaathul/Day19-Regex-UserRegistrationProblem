cdd7cdb29ef4b1c53719eb7ff093e2076c07f78e
drisyaathul <drisyak809@gmail.com>
2023-03-29 15:14:11 +0800

    Added: Valid Last Name

--- src/com/bridgelabz/UserRegistrationProblem.java
+++ src/com/bridgelabz/UserRegistrationProblem.java
@@ -6,12 +6,19 @@ import java.util.regex.Pattern;
 public class UserRegistrationProblem {
     public static void main(String[] args) {
         System.out.println(" User Registration Problem - Regex ");
-
-        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
-        Matcher matcher = pattern.matcher("Drisya");
-        if (matcher.matches())
-            System.out.println("valid FirstName");
+//UC1:
+        Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{3,}");
+        Matcher matcher1 = pattern1.matcher("Drisya");
+        if (matcher1.matches())
+            System.out.println("Valid FirstName");
         else
             System.out.println("Invalid FirstName");
+//UC2:
+        Pattern pattern2 = Pattern.compile("^[A-Z][a-z]{3,}");
+        Matcher matcher2 = pattern2.matcher("Athul");
+        if (matcher2.matches())
+            System.out.println("Valid LastName");
+        else
+            System.out.println("Invalid LastName");
     }
 }
