86a88b83433a4e1dfef19214b9ecf688b18f3001
drisyaathul <drisyak809@gmail.com>
2023-03-29 14:49:58 +0800

    Added: Valid First Name

--- src/com/bridgelabz/UserRegistrationProblem.java
+++ src/com/bridgelabz/UserRegistrationProblem.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistrationProblem {
     public static void main(String[] args) {
         System.out.println(" User Registration Problem - Regex ");
+
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
+        Matcher matcher = pattern.matcher("Drisya");
+        if (matcher.matches())
+            System.out.println("valid FirstName");
+        else
+            System.out.println("Invalid FirstName");
     }
 }
